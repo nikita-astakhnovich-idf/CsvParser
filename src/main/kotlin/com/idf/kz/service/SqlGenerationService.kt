7d715d4054e2a267d50fbdc4dd4593220446e149
korzdan <daniil.korzun@idftechnology.by>
2022-11-18 14:08:28 +0300

    feat(slkz-nikita-parser): add update query generator

--- src/main/kotlin/com/idf/kz/service/SqlGenerationService.kt
+++ src/main/kotlin/com/idf/kz/service/SqlGenerationService.kt
@@ -1,33 +1,32 @@
 package com.idf.kz.service
 
-import com.idf.kz.model.SettlementDB
 import com.idf.kz.model.SettlementInsertDB
-import java.lang.StringBuilder
+import com.idf.kz.model.SettlementUpdateDB
+import kotlin.text.StringBuilder
 
 class SqlGenerationService {
 
-  fun generateInsertSql(insertList: List<SettlementInsertDB>, tableName: String):String {
-    val insertSql =
-      "INSERT INTO $tableName (region_id, address_district_id, address_settlement_type_id, name, kato_id, " +
-          "parent_name, resource)\nVALUES "
-    val fullSqlScript = StringBuilder(insertSql)
+  fun generateInsertSql(insertList: List<SettlementInsertDB>, tableName: String): String {
+    val insertSQLScript =
+        "INSERT INTO $tableName (region_id, address_district_id, address_settlement_type_id, name, kato_id, " +
+            "parent_name, resource)\nVALUES "
+    val fullSqlScript = StringBuilder(insertSQLScript)
     insertList.forEach {
       fullSqlScript.append("(${it.regionId}, ${it.addressDistrictId}, ${it.addressSettlementTypeId}, ${it.name}, " +
           "${it.katoId}, ${it.parentName}, ${it.resource}),\n")
     }
-    fullSqlScript.delete(fullSqlScript.length-2, fullSqlScript.length-1)
+    fullSqlScript.delete(fullSqlScript.length - 2, fullSqlScript.length - 1)
     return fullSqlScript.toString()
   }
 
-  fun generateUpdateSql(updateList: List<SettlementDB>) {
+  fun generateUpdateSql(updateList: List<SettlementUpdateDB>, tableName: String): String {
+    val updateSQLScript = StringBuilder();
+    updateList.forEach {
+      updateSQLScript.append("UPDATE $tableName\n" +
+          "SET address_settlement_type_id = ${it.addressSettlementTypeId}, kato_id = ${it.katoId}," +
+          " parent_name = ${it.parentName}\n" +
+          "WHERE id = ${it.id} AND name = ${it.name};\n\n")
+    }
+    return updateSQLScript.toString()
   }
-}
-
-fun main() {
-  val s1 = SettlementInsertDB(123, 123, 123, "qwe", 123, "123", "123")
-  val s2 = SettlementInsertDB(123, 123, 123, "qwe", 123, "123", "123")
-  val s3 = SettlementInsertDB(123, 123, 123, "qwe", 123, "123", "123")
-  val s4 = SettlementInsertDB(123, 123, 123, "qwe", 123, "123", "123")
-  println(SqlGenerationService().generateInsertSql(listOf(s1, s2, s3, s4), "address_settelment"))
-  println(SqlGenerationService().generateInsertSql(listOf(s1, s2, s3, s4), "address_settelment_kato"))
 }
