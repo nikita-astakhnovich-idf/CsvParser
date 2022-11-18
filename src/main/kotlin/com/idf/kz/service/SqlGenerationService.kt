030ee1ff39e1a6be2472c62e2fb961c38a20b7f5
nikita-astakhnovich-idf <nikita.astahnovich@idftechnology.by>
2022-11-18 13:59:57 +0300

    fix insert method

--- src/main/kotlin/com/idf/kz/service/SqlGenerationService.kt
+++ src/main/kotlin/com/idf/kz/service/SqlGenerationService.kt
@@ -6,17 +6,28 @@ import java.lang.StringBuilder
 
 class SqlGenerationService {
 
-  fun generateInsertSql(insertList: List<SettlementInsertDB>) {
+  fun generateInsertSql(insertList: List<SettlementInsertDB>, tableName: String):String {
     val insertSql =
-      "INSERT INTO address_settlement (region_id, address_district_id, address_settlement_type_id, name, kato_id, " +
-          "parent_name, resource) VALUES"
+      "INSERT INTO $tableName (region_id, address_district_id, address_settlement_type_id, name, kato_id, " +
+          "parent_name, resource)\nVALUES "
     val fullSqlScript = StringBuilder(insertSql)
     insertList.forEach {
-      fullSqlScript.append("(${it.regionId}, ${it.addressDistrictId}, ${it.addressSettlementTypeId}, ${it.name}," +
+      fullSqlScript.append("(${it.regionId}, ${it.addressDistrictId}, ${it.addressSettlementTypeId}, ${it.name}, " +
           "${it.katoId}, ${it.parentName}, ${it.resource}),\n")
     }
+    fullSqlScript.delete(fullSqlScript.length-2, fullSqlScript.length-1)
+    return fullSqlScript.toString()
   }
 
   fun generateUpdateSql(updateList: List<SettlementDB>) {
   }
+}
+
+fun main() {
+  val s1 = SettlementInsertDB(123, 123, 123, "qwe", 123, "123", "123")
+  val s2 = SettlementInsertDB(123, 123, 123, "qwe", 123, "123", "123")
+  val s3 = SettlementInsertDB(123, 123, 123, "qwe", 123, "123", "123")
+  val s4 = SettlementInsertDB(123, 123, 123, "qwe", 123, "123", "123")
+  println(SqlGenerationService().generateInsertSql(listOf(s1, s2, s3, s4), "address_settelment"))
+  println(SqlGenerationService().generateInsertSql(listOf(s1, s2, s3, s4), "address_settelment_kato"))
 }
