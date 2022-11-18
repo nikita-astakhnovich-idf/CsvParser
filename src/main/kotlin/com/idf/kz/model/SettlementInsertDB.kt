2e55c2d1ea4d9417adadf18a596a50ef533be323
korzdan <daniil.korzun@idftechnology.by>
2022-11-18 14:35:46 +0300

    fix: make kato_id in SettlementInsertDB Long type

--- src/main/kotlin/com/idf/kz/model/SettlementInsertDB.kt
+++ src/main/kotlin/com/idf/kz/model/SettlementInsertDB.kt
@@ -1,10 +1,10 @@
 package com.idf.kz.model
 
 data class SettlementInsertDB(
-  val regionId: Int,
-  val addressDistrictId: Int,
-  val addressSettlementTypeId: Int,
+  val regionId: Long,
+  val addressDistrictId: Long,
+  val addressSettlementTypeId: Long,
   val name: String,
-  val katoId: Int,
+  val katoId: Long,
   val parentName: String,
   val resource: String)
