bdf767183d9b620a2b3a99e43ad594980f06355c
nikita-astakhnovich-idf <nikita.astahnovich@idftechnology.by>
2022-11-23 14:46:42 +0300

    fix

--- src/main/kotlin/com/idf/kz/Main.kt
+++ src/main/kotlin/com/idf/kz/Main.kt
@@ -2,7 +2,20 @@ package com.idf.kz
 
 import com.idf.kz.service.ParseService
 import com.idf.kz.service.SqlGenerationService
+import com.idf.kz.service.VerificationManualModelService
 
 fun main() {
-  println(SqlGenerationService().generateUpdateSql(ParseService().getUpdateSettlement()))
+  ParseService().getUpdateSettlement()
+  val s = VerificationManualModelService(
+    ParseService.manualList,
+    ParseService.updateSettlements,
+    ParseService.settlementsFromProd
+  )
+
+//  ParseService.updateSettlements.forEach { println(it) }
+  println("////////////////////////////////////////////////////////////////////////////////////////////////////////")
+  println(s.findUncheckedModel().size)
+  s.findNewDistrictModel().forEach{ println(it) }
+  println("//////////////////////////////////////////////////////")
+  VerificationManualModelService.checkedList.forEach { println(it) }
 }
