6ef2426447554b664e022addb51a269a312aca5c
Andrei Shkurko <andrei.shkurko@idftechnology.by>
2022-11-23 14:47:30 +0300

    feat added Алматинская область parser

--- src/main/kotlin/com/idf/kz/Main.kt
+++ src/main/kotlin/com/idf/kz/Main.kt
@@ -4,5 +4,14 @@ import com.idf.kz.service.ParseService
 import com.idf.kz.service.SqlGenerationService
 
 fun main() {
-  println(SqlGenerationService().generateUpdateSql(ParseService().getUpdateSettlement()))
+//  println(SqlGenerationService().generateUpdateSql(ParseService().getUpdateSettlement()))
+
+  val parseService = ParseService()
+  parseService.getUpdateSettlement()
+  println("all in districts  ${ParseService.districts.flatMap { it.settlements }.count()}")
+  println("manual ${ParseService.manualList.size}")
+  println("manualMoreOne ${ParseService.manualListMoreOne.size}")
+  println("update ${ParseService.updateSettlements.size}")
+  println("repeatable ${ParseService.repeatableUpdateSettlements.size}")
+//  ParseService.manualList.forEach { println(it) }
 }
