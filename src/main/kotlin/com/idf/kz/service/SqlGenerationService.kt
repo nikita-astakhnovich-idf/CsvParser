59a39ad4dc372091bebabb6d72fedc4623981d47
nikita-astakhnovich-idf <nikita.astahnovich@idftechnology.by>
2022-12-01 10:28:52 +0300

    fix

--- src/main/kotlin/com/idf/kz/service/SqlGenerationService.kt
+++ src/main/kotlin/com/idf/kz/service/SqlGenerationService.kt
@@ -8,28 +8,8 @@ class SqlGenerationService {
   fun generateFullInsertSql(insertList: List<InsertSettlement>): String {
     val fullSqlScript = StringBuilder()
     insertList.forEach {
-      fullSqlScript.append(
-        "INSERT INTO address_settlement (region_id, address_district_id, address_settlement_type_id," +
-            " name, kato_id, parent_name, resource)" +
-            "\nVALUES (" +
-            "REGION_ID_ABAI, " +
-            "${it.addressDistrictId}, " +
-            "${it.addressSettlementTypeId}, " +
-            "'${it.name}', " +
-            "${it.katoId}, " +
-            "'${it.parentName}', " +
-            "${it.resource})" +
-            "\n\n"
-      )
-
-      fullSqlScript.append(
-        "INSERT INTO address_settlement_kato (kato_version, address_settlement_id, kato_id)" +
-            "\nVALUES (" +
-            "'1', " +
-            "(SELECT LAST_INSERT_ID()), " +
-            "${it.katoId})" +
-            "\n\n\n"
-      )
+      fullSqlScript.append(getInsertAddressSettlement(it))
+      fullSqlScript.append(getInsertAddressSettlementKato(it))
     }
     fullSqlScript.delete(fullSqlScript.length - 2, fullSqlScript.length - 1)
     return fullSqlScript.toString()
@@ -49,7 +29,7 @@ class SqlGenerationService {
     updateList.forEach {
       updateSQLScript.append(getUpdateAddressSettlement(it))
       updateSQLScript.append(getUpdateAddressSettlementKato(it))
-      if (ParseService.setSettlementsId.contains(it.id)){
+      if (ParseService.setSettlementsId.contains(it.id)) {
         updateSQLScript.append(getUpdateAddress(it))
       }
     }
@@ -67,7 +47,30 @@ class SqlGenerationService {
     return updateSQLScript.toString()
   }
 
-  private fun getUpdateAddressSettlement(updateSettlement: UpdateSettlement): String{
+  private fun getInsertAddressSettlement(insertSettlement: InsertSettlement): String {
+    return "INSERT INTO address_settlement (region_id, address_district_id, address_settlement_type_id," +
+        " name, kato_id, parent_name, resource)" +
+        "\nVALUES (" +
+        "REGION_ID_ABAI, " +
+        "${insertSettlement.addressDistrictId}, " +
+        "${insertSettlement.addressSettlementTypeId}, " +
+        "'${insertSettlement.name}', " +
+        "${insertSettlement.katoId}, " +
+        "'${insertSettlement.parentName}', " +
+        "${insertSettlement.resource});" +
+        "\n\n"
+  }
+
+  private fun getInsertAddressSettlementKato(insertSettlement: InsertSettlement): String {
+    return "INSERT INTO address_settlement_kato (kato_version, address_settlement_id, kato_id)" +
+        "\nVALUES (" +
+        "'1', " +
+        "(SELECT LAST_INSERT_ID()), " +
+        "${insertSettlement.katoId});" +
+        "\n\n\n"
+  }
+
+  private fun getUpdateAddressSettlement(updateSettlement: UpdateSettlement): String {
     return "UPDATE address_settlement \n" +
         "SET region_id = REGION_ID_ABAI, " +
         "address_settlement_type_id = ${updateSettlement.addressSettlementTypeId}, " +
@@ -76,20 +79,20 @@ class SqlGenerationService {
         "WHERE id = ${updateSettlement.id};\n\n"
   }
 
-  private fun getUpdateAddressSettlementKato(updateSettlement: UpdateSettlement): String{
+  private fun getUpdateAddressSettlementKato(updateSettlement: UpdateSettlement): String {
     return "UPDATE address_settlement_kato \n" +
         "SET kato_id = ${updateSettlement.katoId} \n" +
         "WHERE address_settlement_id = ${updateSettlement.id} AND kato_version = '1';\n\n"
   }
 
-  private fun getUpdateAddress(updateSettlement: UpdateSettlement): String{
+  private fun getUpdateAddress(updateSettlement: UpdateSettlement): String {
     return "UPDATE address \n" +
         "SET region_id = REGION_ID_ABAI, \n" +
         "district_id = (SELECT id FROM address_district WHERE name = '${updateSettlement.districtName}')\n" +
         "WHERE settlement_id = ${updateSettlement.id};\n\n"
   }
 
-  private fun getUpdateAddressSettlementAksuat(updateSettlement: UpdateSettlement): String{
+  private fun getUpdateAddressSettlementAksuat(updateSettlement: UpdateSettlement): String {
     return "UPDATE address_settlement \n" +
         "SET address_district_id = ADDRESS_DISTRICT_ID_AKSUAT, " +
         "region_id = REGION_ID_ABAI, " +
@@ -99,13 +102,13 @@ class SqlGenerationService {
         "WHERE id = ${updateSettlement.id};\n\n"
   }
 
-  private fun getUpdateAddressSettlementKatoAksuat(updateSettlement: UpdateSettlement): String{
+  private fun getUpdateAddressSettlementKatoAksuat(updateSettlement: UpdateSettlement): String {
     return "UPDATE address_settlement_kato \n" +
         "SET kato_id = ${updateSettlement.katoId} \n" +
         "WHERE address_settlement_id = ${updateSettlement.id} AND kato_version = '1';\n\n"
   }
 
-  private fun getDelimiter():String{
+  private fun getDelimiter(): String {
     return "\n" +
         "\n" +
         "\n" +
